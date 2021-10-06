4572aa5ac8251b6cc5e4865d29a85a6a72c71b9b
sumin416 <sumin416@naver.com>
2021-10-06 21:56:38 +0900

    컴퓨터와 사용자의 값을 비교해 볼 개수 파악하는 기능

--- src/main/java/baseball/Score.java
+++ src/main/java/baseball/Score.java
@@ -5,19 +5,33 @@ public class Score {
     private int ball;
 
     public Score() {
+        initScore();
+    }
+
+    private void initScore() {
         strike = 0;
         ball = 0;
     }
 
     void calScore(String computeValue, String userValue) {
+        initScore();
 
         calStrike(computeValue, userValue);
+        calBall(computeValue, userValue);
     }
-
 
     private void calStrike(String computeValue, String userValue) {
         for (int i = 0; i < 3; i++) {
             if (computeValue.charAt(i) == userValue.charAt(i)) ++strike;
+        }
+    }
+
+    private void calBall(String computeValue, String userValue) {
+        final int LEN = computeValue.length();
+        for (int i = 0; i < LEN; i++) {
+            for (int j = 0; j < LEN; j++) {
+                if (i != j && computeValue.charAt(i) == userValue.charAt(j)) ++ball;
+            }
         }
     }
 
--- src/test/java/baseball/ScoreTest.java
+++ src/test/java/baseball/ScoreTest.java
@@ -1,9 +1,18 @@
 package baseball;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ScoreTest {
+
+    private Score score;
+
+    @BeforeEach
+    void beforeEach() {
+        score = new Score();
+    }
+
     @Test
     void 스트라이크_점수가_맞게_계산되는지_검증() {
         // GIVEN
@@ -11,10 +20,22 @@ public class ScoreTest {
         String userValue = "157";
 
         // WHEN
-        Score score = new Score();
         score.calScore(computerValue, userValue);
 
         // THEN
         Assertions.assertEquals(2, score.getStrike());
+    }
+
+    @Test
+    void 볼_점수가_맞게_계산되는지_검증() {
+        // GIVEN
+        String computerValue = "127";
+        String userValue = "732";
+
+        // WHEN
+        score.calScore(computerValue, userValue);
+
+        // THEN
+        Assertions.assertEquals(2, score.getBall());
     }
 }
